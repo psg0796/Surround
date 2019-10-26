499322905a2fb2cfe4398f47910ec99cdbf2fad4
Prashant Shekhar Gupta <guptaprashantshekhar@gmail.com>
2019-10-26 18:24:33 +0530

    started using nativeBase

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { AppLoading } from 'expo';
+import { Container } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import MainApp from './src/MainApp';
 
-export default function App() {
-  return (
-    <MainApp />
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return (
+      <Container>
+        <MainApp />
+      </Container>
+    );
+  }
 }
